bffb1b9835923e95b4f1ef396c960e84e443900d
miaojiayi <2448735803@qq.com>
2025-12-11 17:02:15 +0800

    fix: downgrade react and update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['pino', 'thread-stream'],
 };
 
 export default nextConfig;
